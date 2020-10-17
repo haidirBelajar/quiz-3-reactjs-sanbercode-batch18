918a5216b3bd43fba054af9cb18506341e95cbcb
haidirBelajar <haidirreact@gmail.com>
2020-10-17 22:40:20 +0700

    quiz3

---
+++ quiz3/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomeView from './home.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomeView />
+      </header>
+    </div>
+  );
+}
+
+export default App;
